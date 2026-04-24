6d095530ea0bc875ee640ec1c22d152b79cc6736
Hamza Anver <hamzaanver1234@gmail.com>
2026-04-24 19:14:45 +0530

    Revert "WIP added plc_get_key_state for future fuzzers"
    This reverts commit 0286e046770e7e978bfe0e979844d29f8b9f69ca.

--- src/common.rs
+++ src/common.rs
@@ -8,5 +8,4 @@ unsafe extern "C" {
 
     // Greybox tools
     pub fn plc_get_full_state(out_buffer: *mut u8, max_size: usize) -> usize;
-    pub fn plc_get_key_state(out_buffer: *mut u8, max_size: usize) -> usize;
 }
